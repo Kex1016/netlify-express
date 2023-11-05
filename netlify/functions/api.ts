e8f09abf2d4f3462fc6ee09484a2799ae5e290dd
cakes <cakes@haiiro.moe>
2023-11-05 23:01:20 +0100

    welcard test

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -3,10 +3,15 @@ import serverless from "serverless-http";
 import { ALIndex } from "../pages/anilist";
 import { ALDiscord } from "../pages/anilist/discord";
 import { ALStatsFM } from "../pages/anilist/statsfm";
+import { Index } from "../pages";
+import { config } from "dotenv";
+
+config();
 
 const api = express();
 
 const router = Router();
+router.get("/", (req, res) => Index(req, res));
 router.get("/anilist", (req, res) => ALIndex(req, res));
 router.get("/anilist/discord", (req, res) => ALDiscord(req, res));
 router.get("/anilist/statsfm", (req, res) => ALStatsFM(req, res));
--- netlify/pages/anilist/discord.ts
+++ netlify/pages/anilist/discord.ts
@@ -1,5 +1,30 @@
 import { Response, Request } from "express";
+import { musicCard } from "welcard";
 
 export async function ALDiscord(req: Request, res: Response) {
-  res.send("Hello world!");
+  const discUser = await fetch(
+    `https://discord.com/api/users/147709526357966848`,
+    {
+      headers: {
+        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      },
+    }
+  ).then((res) => res.json());
+  const image = new musicCard();
+
+  const avatar = `https://cdn.discordapp.com/avatars/${discUser.id}/${discUser.avatar}.png?size=1024`;
+
+  image.setThumbnail(avatar);
+  image.setColor("auto");
+  image.setName(discUser.username);
+  image.setBrightness(50);
+
+  const imageBuffer = await image.build();
+
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+    "Content-Length": imageBuffer.length,
+  });
+
+  res.end(imageBuffer);
 }
