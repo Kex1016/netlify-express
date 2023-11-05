bdc0e94e58bc09ed99957242d0a7da2e0fcd042e
cakes <cakes@haiiro.moe>
2023-11-05 22:35:10 +0100

    idk what im doing

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -11,6 +11,6 @@ router.get("/anilist", (req, res) => ALIndex(req, res));
 router.get("/anilist/discord", (req, res) => ALDiscord(req, res));
 router.get("/anilist/statsfm", (req, res) => ALStatsFM(req, res));
 
-api.use("/", router);
+api.use(router);
 
 export const handler = serverless(api);
