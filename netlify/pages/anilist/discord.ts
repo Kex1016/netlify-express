317bea115fe8b431b5e2b7e0cef0fe7a641e0488
cakes <cakes@haiiro.moe>
2023-11-05 23:03:28 +0100

    failed experiment, back to placeholder

--- netlify/pages/anilist/discord.ts
+++ netlify/pages/anilist/discord.ts
@@ -1,5 +1,4 @@
 import { Response, Request } from "express";
-import { musicCard } from "welcard";
 
 export async function ALDiscord(req: Request, res: Response) {
   const discUser = await fetch(
@@ -10,21 +9,7 @@ export async function ALDiscord(req: Request, res: Response) {
       },
     }
   ).then((res) => res.json());
-  const image = new musicCard();
-
   const avatar = `https://cdn.discordapp.com/avatars/${discUser.id}/${discUser.avatar}.png?size=1024`;
 
-  image.setThumbnail(avatar);
-  image.setColor("auto");
-  image.setName(discUser.username);
-  image.setBrightness(50);
-
-  const imageBuffer = await image.build();
-
-  res.writeHead(200, {
-    "Content-Type": "image/png",
-    "Content-Length": imageBuffer.length,
-  });
-
-  res.end(imageBuffer);
+  res.send(`hello world! <img src="${avatar}" />`);
 }
