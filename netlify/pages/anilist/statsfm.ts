dc8ee70f9983c39a700fd78ec4bdfb1d71ee1d22
cakes <nyilasdavid@gmail.com>
2023-11-06 11:09:32 +0100

    more statsfm

--- netlify/pages/anilist/statsfm.ts
+++ netlify/pages/anilist/statsfm.ts
@@ -1,8 +1,14 @@
-import { Response, Request } from "express";
+import {Response, Request} from "express";
 
 export async function ALStatsFM(req: Request, res: Response) {
   const apiBase = "https://beta-api.stats.fm";
-  const test = await fetch(`${apiBase}/api/v1/users/the_cakes/profile`).then((res) => res.json());
-  
-  res.send(test);
+  const profile = await fetch(`${apiBase}/api/v1/users/the_cakes/profile`).then((res) => res.json());
+  const streams = await fetch(`${apiBase}/api/v1/users/the_cakes/streams`).then((res) => res.json());
+  const current = await fetch(`${apiBase}/api/v1/users/the_cakes/streams/current`).then((res) => res.json());
+
+  const response = {
+    profile, streams, current
+  }
+
+  res.send(response);
 }
