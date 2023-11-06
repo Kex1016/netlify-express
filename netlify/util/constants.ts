9660cc4d066a32d154154fa8003bb6751940185c
cakes <nyilasdavid@gmail.com>
2023-11-06 11:15:20 +0100

    get rid of  stupid header

--- netlify/util/constants.ts
+++ netlify/util/constants.ts
@@ -1,45 +1,44 @@
-import { Response, Request } from "express";
+import {Response, Request} from "express";
+
 const html = String.raw;
 
-export const errorHtml = (req: Request, res: Response) => html`<html>
-  <head>
-    <title>Anilist</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <style></style>
-    <title>Hello there</title>
-  </head>
-  <body>
+export const errorHtml = (req: Request, res: Response) => html`
+    <html>
+    <head>
+        <title>Anilist</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <style></style>
+        <title>Hello there</title>
+    </head>
+    <body>
     <header>
-      <code>
-        <pre>
-      ${JSON.stringify(req.headers, null, 2)}
-    </pre
-        >
-      </code>
+        <code>
+            bruh moment
+        </code>
     </header>
 
     <main>
-      <h1>You are not supposed to be here!</h1>
-      <h2>Check out what's here then I guess:</h2>
-      <h3>
-        DISCLAIMER: THIS IS EXTREMELY EARLY IN DEV. CHECK OUT GH REPO IN THE
-        FOOTER. NOTHING IS WORKING YET.
-      </h3>
+        <h1>You are not supposed to be here!</h1>
+        <h2>Check out what's here then I guess:</h2>
+        <h3>
+            DISCLAIMER: THIS IS EXTREMELY EARLY IN DEV. CHECK OUT GH REPO IN THE
+            FOOTER. NOTHING IS WORKING YET.
+        </h3>
 
-      <ul>
-        <li>
-          <a href="/anilist/discord">discord profile svg</a>
-        </li>
-        <li>
-          <a href="/anilist/statsfm">stats.fm profile svg</a>
-        </li>
-      </ul>
+        <ul>
+            <li>
+                <a href="/anilist/discord">discord profile svg</a>
+            </li>
+            <li>
+                <a href="/anilist/statsfm">stats.fm profile svg</a>
+            </li>
+        </ul>
     </main>
 
     <footer>
-      <p>
-        <a href="https://github.com/Kex1016/netlify-express">github</a>
-      </p>
+        <p>
+            <a href="https://github.com/Kex1016/netlify-express">github</a>
+        </p>
     </footer>
-  </body>
-</html>`;
+    </body>
+    </html>`;
