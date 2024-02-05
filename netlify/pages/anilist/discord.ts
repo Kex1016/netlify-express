9dc2ea54b41bd065785c58cd74bb1d1ca214652d
Kex (David) <nyilasdavid@gmail.com>
2024-02-05 11:59:17 +0100

    Cache testing
    Added a cache to test if it works with Netlify

--- netlify/pages/anilist/discord.ts
+++ netlify/pages/anilist/discord.ts
@@ -1,6 +1,16 @@
 import { Response, Request } from "express";
 
+import { Cache } from "memory-cache";
+
+const cache = new Cache();
+
 export async function ALDiscord(req: Request, res: Response) {
+  const cached = cache.get("discord");
+  if (cached) {
+    res.send(cached);
+    return;
+  }
+
   const discUser = await fetch(
     `https://discord.com/api/users/147709526357966848`,
     {
@@ -11,6 +21,13 @@ export async function ALDiscord(req: Request, res: Response) {
   ).then((res) => res.json());
   const avatar = `https://cdn.discordapp.com/avatars/${discUser.id}/${discUser.avatar}.png?size=1024`;
 
+  const response = {
+    ...discUser,
+    avatarUrl: avatar,
+  };
+
+  cache.put("discord", response, 1000 * 60);
+
   // TODO: Make this an svg, and send it as an image or whatever
-  res.send(`hello world!<br>discord avatar:<br><img src="${avatar}" />`);
+  res.send(response);
 }
--- netlify/pages/anilist/statsfm.ts
+++ netlify/pages/anilist/statsfm.ts
@@ -1,14 +1,34 @@
-import {Response, Request} from "express";
+import { Response, Request } from "express";
+
+import { Cache } from "memory-cache";
+
+const cache = new Cache();
 
 export async function ALStatsFM(req: Request, res: Response) {
+  const cached = cache.get("statsfm");
+  if (cached) {
+    res.send(cached);
+    return;
+  }
+
   const apiBase = "https://beta-api.stats.fm";
-  const profile = await fetch(`${apiBase}/api/v1/users/the_cakes/profile`).then((res) => res.json());
-  const streams = await fetch(`${apiBase}/api/v1/users/the_cakes/streams`).then((res) => res.json());
-  const current = await fetch(`${apiBase}/api/v1/users/the_cakes/streams/current`).then((res) => res.json());
+  const profile = await fetch(`${apiBase}/api/v1/users/the_cakes/profile`).then(
+    (res) => res.json()
+  );
+  const streams = await fetch(`${apiBase}/api/v1/users/the_cakes/streams`).then(
+    (res) => res.json()
+  );
+  const current = await fetch(
+    `${apiBase}/api/v1/users/the_cakes/streams/current`
+  ).then((res) => res.json());
 
   const response = {
-    profile, streams, current
-  }
+    profile,
+    streams,
+    current,
+  };
+
+  cache.put("statsfm", response, 1000 * 60);
 
   res.send(response);
 }
